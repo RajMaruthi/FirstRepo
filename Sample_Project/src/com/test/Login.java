eee637cadf422ecdf0813dad2637a2aa1ae16d55
Maruthi <maruthirajabn@gmail.com>
2022-02-19 19:22:47 +0530

    First Commit

---
+++ Sample_Project/src/com/test/Login.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Login {
+
+	public static void main(String[] args) 
+	{
+		
+		login();
+
+	}
+	
+	static void login()
+	{
+		System.out.println("Login is Successfull");
+	}
+
+}
